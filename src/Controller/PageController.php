09b6a881145b2ac9f4cba9b627bef5383f4bd38f
Miguel Figueroa <miguelfigueroa90@gmail.com>
2025-08-05 10:41:02 -0400

    Initial commit

---
+++ src/Controller/PageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('pages/index.html.twig', [
+            'title' => 'Home Page',
+        ]);
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('pages/about.html.twig', [
+            'title' => 'About Us'
+        ]);
+    }
+}
