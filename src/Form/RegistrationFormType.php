9c0e597dc2d155132e201370fa473cbca5b072e3
Miguel Figueroa <miguelfigueroa90@gmail.com>
2025-08-12 10:04:23 -0400

    Actualizando formulario de registro

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,8 +19,12 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
+            ->add('name', TextType::class, [
+                'attr' => ['class' => 'mt-2 mb-4 h-10 p-2 w-full rounded border-gray-300 shadow-sm sm:text-sm dark:border-stone-50 dark:bg-stone-200 dark:text-gray-600']
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => ['class' => 'mt-2 mb-4 h-10 p-2 w-full rounded border-gray-300 shadow-sm sm:text-sm dark:border-stone-50 dark:bg-stone-200 dark:text-gray-600']
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -26,12 +32,16 @@ class RegistrationFormType extends AbstractType
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
+                'attr' => ['class' => 'ml-4 mb-4']
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'mt-2 mb-4 h-10 p-2 w-full rounded border-gray-300 shadow-sm sm:text-sm dark:border-stone-50 dark:bg-stone-200 dark:text-gray-600',
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
